cffc22c64181a7827ef609a3c30cc399cc764c1c
ssarandev <samuil.sarandev@dxc.com>
2023-11-16 22:20:38 +0200

    Add responsive SignIn errors

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -27,10 +27,10 @@ function SignIn() {
 		try {
 			await signIn(userData);
 		} catch (error: any) {
-			if (error.message === 'Invalid credentials!') {
-				setError('Invalid credentials!');
+			console.log(error.message);
+			if (error.message === 'Invalid credentials') {
+				setError('Invalid credentials');
 			} else {
-				console.error(error.message);
 				setError(
 					'Could not sign in right now. Please try again later!'
 				);
@@ -49,39 +49,54 @@ function SignIn() {
 					onSubmit={handleSubmit}
 				>
 					<div className='mt-2 mb-4'>
-						<FormInput
-							label='Email'
-							isInvalid={!!error}
-						>
+						<FormInput label='Email'>
 							<input
 								type='text'
 								id='email'
 								className={
 									'input input-bordered w-full max-w-xs text-base-content' +
-									(!!error ? ' border-2 border-error' : '')
+									(!!error && !userData.email
+										? ' border-2 border-error'
+										: '')
 								}
 								onChange={handleChange}
 							/>
 						</FormInput>
-						<FormInput
-							label='Password'
-							isInvalid={!!error}
-						>
+						<FormInput label='Password'>
 							<input
 								type='password'
 								id='password'
 								className={
 									'input input-bordered w-full max-w-xs text-base-content' +
-									(!!error ? ' border-2 border-error' : '')
+									(!!error && !userData.password
+										? ' border-2 border-error'
+										: '')
 								}
 								onChange={handleChange}
 							/>
 						</FormInput>
 					</div>
 					{error && (
-						<div className='text-start text-error'>{error}</div>
+						<div className='flex justify-start'>
+							<div className='badge badge-error'>
+								{error}
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									className='inline-block w-4 h-4 stroke-current'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth='2'
+										d='M6 18L18 6M6 6l12 12'
+									></path>
+								</svg>
+							</div>
+						</div>
 					)}
-					<button className='btn btn-accent btn-block'>
+					<button className='btn btn-accent btn-block mt-4'>
 						Sign In
 					</button>
 				</form>
