558843a690851ea5244357b2d713de4468457015
ssarandev <samuil.sarandev@dxc.com>
2023-11-23 13:22:00 +0200

    Fix InfoHero card styles

--- src/components/landing-page/InfoHero/InfoHero.tsx
+++ src/components/landing-page/InfoHero/InfoHero.tsx
@@ -17,19 +17,20 @@ const InfoHero = () => {
 
 	return (
 		<div className='hero py-14 bg-base-200'>
-			<div className='hero-content flex-col xl:flex-row'>
+			<div className='hero-content flex-col xl:grid xl:grid-cols-3'>
 				<BaseCard
 					title='Travel Stress-Free'
-					cardStyles={`shadow-xl ${styles.hidden} bg-primary text-primary-content`}
-					image='https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
+					cardStyles={`shadow-xl ${styles.hidden} bg-primary text-primary-content h-full`}
+					image='https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg'
 				>
 					<div className='py-4'>
 						<p>If a dog chews shoes whose shoes does he choose?</p>
 					</div>
 				</BaseCard>
+
 				<BaseCard
 					title='Rent Gear'
-					cardStyles={`shadow-xl ${styles.hidden}`}
+					cardStyles={`shadow-xl ${styles.hidden} h-full`}
 					image='https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
 					imageBottom
 				>
@@ -39,7 +40,7 @@ const InfoHero = () => {
 				</BaseCard>
 				<BaseCard
 					title='Clean and Trusty'
-					cardStyles={`shadow-xl ${styles.hidden} bg-secondary text-secondary-content`}
+					cardStyles={`shadow-xl ${styles.hidden} bg-secondary text-secondary-content h-full`}
 					image='https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
 				>
 					<div className='py-4'>
