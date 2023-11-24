b31538cff79c2d9cdb08af7bcd0af3d1266c8d91
ssarandev <samuil.sarandev@dxc.com>
2023-11-24 11:13:03 +0200

    Extend staggerAnimation util

--- src/utils/animation.utils.ts
+++ src/utils/animation.utils.ts
@@ -1,11 +1,19 @@
 export const staggerAnimation = (
 	showClass: string,
-	elems: NodeListOf<Element>
+	elems: NodeListOf<Element>,
+	hiddenClass?: string,
+	afterTransitionClass?: string
 ) => {
 	const observer = new IntersectionObserver((entries) => {
 		entries.forEach((entry) => {
 			if (entry.isIntersecting) {
 				entry.target.classList.add(showClass);
+				hiddenClass &&
+					setTimeout(() => {
+						entry.target.classList.remove(hiddenClass);
+						afterTransitionClass &&
+							entry.target.classList.add(afterTransitionClass);
+					}, 2000);
 			}
 		});
 	});
