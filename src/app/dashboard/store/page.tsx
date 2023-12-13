d7fd9c10c50fb0559bf29b76c37ceed72254a80e
ssarandev <samuil.sarandev@dxc.com>
2023-12-13 16:07:46 +0200

    Add base StoreHeader component and dashboard store page

--- src/app/dashboard/store/page.tsx
+++ src/app/dashboard/store/page.tsx
@@ -1,7 +1,12 @@
+import StoreHeader from '@/components/store/StoreHeader/StoreHeader';
 import React from 'react';
 
 const Store = () => {
-	return <div className='w-full min-h-full px-4'>Store</div>;
+	return (
+		<div className='w-full min-h-full px-4'>
+			<StoreHeader />
+		</div>
+	);
 };
 
 export default Store;
